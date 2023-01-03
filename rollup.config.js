6036d5acc6a5c34dd98ea60358e64d4894fb16a4
gerardo.caceres <gerardo.caceres@cencosudscotiabank.cl>
2023-01-03 14:27:51 -0300

    Added rollup to export storybook as a library

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-anonymous-default-export */
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+
+const packageJson = require("./package.json");
+
+export default {
+  input: "stories/index.ts",
+  output: [
+    {
+      file: "build/index.js",
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: "build/index.es.js",
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './rollup.tsconfig.json' }),
+    postcss()
+  ]
+};
